04a44c8f2b6b5425acea98f34f3ff56dd73a86db
Lucas <lcseferreira@hotmail.com>
2022-02-20 01:02:04 -0300

    2 - Primeiros passos com .NET - Estrutura de um programa C#

---
+++ 2 - Primeiros passos com .NET/EstruturaDoPrograma/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace EstruturaDoPrograma
+{
+    class Program
+    {
+        static void Main()
+        {
+            var minhaPilha = new Pilha();
+
+            minhaPilha.Empilhar(2);
+            minhaPilha.Empilhar(6);
+            minhaPilha.Empilhar(4);
+            System.Console.WriteLine(minhaPilha.Desempilhar());
+            System.Console.WriteLine(minhaPilha.Desempilhar());
+            System.Console.WriteLine(minhaPilha.Desempilhar());
+        }
+    }
+}
