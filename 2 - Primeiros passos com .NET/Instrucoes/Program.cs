5bd3b8ca266f2b31c1448cff8e4fffbe7651b7ef
Lucas <lcseferreira@hotmail.com>
2022-02-20 11:49:37 -0300

    2 - Primeiros passos com .NET - Declarações de variáveis e IF

---
+++ 2 - Primeiros passos com .NET/Instrucoes/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace Instrucoes
+{
+    class Program
+    {
+        static void Main()
+        {
+            string[] meuArrayDeNomes = { "Lucas", "Eduardo", "Renata" };
+
+            Declaracoes();
+            InstrucaoIf(meuArrayDeNomes);
+        }
+
+        static void Declaracoes()
+        {
+            int a;
+            int b = 2, c = 3;
+            const int d = 4;
+            a = 1;
+            Console.WriteLine(a + b + c + d);
+        }
+
+        static void InstrucaoIf(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                Console.WriteLine("Nenhum argumento.");
+            }
+            else if (args.Length == 1)
+            {
+                Console.WriteLine("1 argumento.");
+            }
+            else
+            {
+                Console.WriteLine($"{args.Length} argumentos.");
+            }
+        }
+    }
+}
