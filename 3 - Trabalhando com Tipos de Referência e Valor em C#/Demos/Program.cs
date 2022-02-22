fe4bf8b45a2f7660b6e2fb1c9642309bffd249a7
Lucas <lcseferreira@hotmail.com>
2022-02-22 15:39:27 -0300

    3 - Trabalhando com Tipos de Referência e Valor - Tipo Valor

--- 3 - Trabalhando com Tipos de Referência e Valor em C#/Demos/Program.cs
+++ 3 - Trabalhando com Tipos de Referência e Valor em C#/Demos/Program.cs
@@ -1,9 +1,18 @@
 ﻿using static System.Console;
 
-class Program
+partial class Program
 {
     static void Main()
     {
-        WriteLine("Hello World");
+        // Value Type int - cópia por valor (alocação na stack)
+        int a = 2;
+        Adicionar20(a);
+
+        WriteLine($"O valor da variável a é {a}");
+
+        // Atribuindo outro valor para a variável a
+        a = Adicionar30(a);
+
+        WriteLine($"O valor da variável a é {a}");
     }
 }
---
+++ 3 - Trabalhando com Tipos de Referência e Valor em C#/Demos/TipoValor.cs
@@ -0,0 +1,12 @@
+partial class Program
+{
+    static void Adicionar20(int a)
+    {
+        a += 20;
+    }
+
+    static int Adicionar30(int x)
+    {
+        return x + 30;
+    }
+}
