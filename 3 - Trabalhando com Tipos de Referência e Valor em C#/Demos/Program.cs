64b4075756eb051f0f18edaa9715fca6a765d867
Lucas <lcseferreira@hotmail.com>
2022-02-23 10:33:26 -0300

    3 - Trabalhando com Tipos de Referência e Valor - Array

--- 3 - Trabalhando com Tipos de Referência e Valor em C#/Demos/Program.cs
+++ 3 - Trabalhando com Tipos de Referência e Valor em C#/Demos/Program.cs
@@ -4,6 +4,11 @@ partial class Program
 {
     static void Main()
     {
-        
+        int[] pares = { 0, 2, 4, 6, 8 };
+
+        MudarParImpar(pares);
+
+        foreach (var n in pares) { WriteLine(n); }
+        WriteLine($"{ string.Join(',', pares)}");
     }
 }
---
+++ 3 - Trabalhando com Tipos de Referência e Valor em C#/Demos/TipoArray.cs
@@ -0,0 +1,11 @@
+partial class Program
+{
+
+    static void MudarParImpar(int[] pares)
+    {
+        for (int i = 0; i < pares.Length; i++)
+        {
+            pares[i] += 1;
+        }
+    }
+}
