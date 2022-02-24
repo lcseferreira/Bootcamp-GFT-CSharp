fb5672cad64874dea40f2b991acbf1160dfd8894
Lucas <lcseferreira@hotmail.com>
2022-02-24 11:23:56 -0300

    3 - Trabalhando com Tipos de Referência e Valor - ref struct

---
+++ 3 - Trabalhando com Tipos de Referência e Valor em C#/DemoStructRef/Program.cs
@@ -0,0 +1,18 @@
+﻿partial class Program
+{
+    static void Main()
+    {
+        Pessoa p1 = new Pessoa();
+        p1.Nome = "Lucas";
+        p1.Idade = 24;
+        p1.EnderecoPessoa = new Endereco()
+        {
+            Logradouro = "Rua Teste",
+            Numero = 32,
+            CEP = "00000",
+            Cidade = "Ribeirão Preto"
+        };
+    }
+
+
+}
