51accfd161c366132313c15b8c2a41d35f948120
Lucas <lcseferreira@hotmail.com>
2022-02-24 10:19:06 -0300

    3 - Trabalhando com Tipos de Referência e Valor - Alterando array de strings

--- 3 - Trabalhando com Tipos de Referência e Valor em C#/DemoRef/Program.cs
+++ 3 - Trabalhando com Tipos de Referência e Valor em C#/DemoRef/Program.cs
@@ -2,8 +2,35 @@
 {
     static void Main()
     {
-        DemoRef();
+        DemoArrayNome();
     }
 
-    
+    static void DemoArrayNome()
+    {
+        var nomes = new String[] { "Lucas", "Eduardo", "Lara", "Renata" };
+
+        Console.WriteLine("Digite o nome a ser substituído: ");
+        var nome = Console.ReadLine();
+
+        Console.WriteLine("Digite o novo nome: ");
+        var nomeNovo = Console.ReadLine();
+
+        AlterarNome(nomes, nome, nomeNovo);
+
+        foreach (var item in nomes)
+        {
+            System.Console.WriteLine(item);
+        }
+
+        static void AlterarNome(string[] nomes, string nome, string nomeNovo)
+        {
+            for (int i = 0; i < nomes.Length; i++)
+            {
+                if (nomes[i] == nome)
+                {
+                    nomes[i] = nomeNovo;
+                }
+            }
+        }
+    }
 }
