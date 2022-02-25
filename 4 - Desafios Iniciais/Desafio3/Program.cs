f3a403a0e74e717a1e65380b5cf62b8b207dbb93
Lucas <lcseferreira@hotmail.com>
2022-02-24 23:27:18 -0300

    4 - Desafios Iniciais - Desafio 3: Soma de pares consecutivos

--- 4 - Desafios Iniciais/Desafio3/Program.cs
+++ 4 - Desafios Iniciais/Desafio3/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Digite um número ou 0 para sair:");
+var numero = int.Parse(Console.ReadLine());
+
+while (numero != 0)
+{
+    int soma, aux;
+
+    if (numero % 2 == 0)
+    {
+        soma = numero;
+        aux = soma + 2;
+        for (int i = 0; i < 4; i++)
+        {
+            soma += aux;
+            aux += 2;
+        }
+        Console.WriteLine(soma);
+    }
+    else
+    {
+        soma = numero + 1;
+        aux = soma + 2;
+        for (int i = 0; i < 4; i++)
+        {
+            soma += aux;
+            aux += 2;
+        }
+        Console.WriteLine(soma);
+    }
+
+    Console.WriteLine("Digite um número ou 0 para sair:");
+    numero = int.Parse(Console.ReadLine());
+}
