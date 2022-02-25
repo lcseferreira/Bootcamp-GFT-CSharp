c7a8e0110d2f3cad055021814c7247fa9c17ae7d
Lucas <lcseferreira@hotmail.com>
2022-02-24 22:51:33 -0300

    4 - Desafios Iniciais - Desafio 2: Quadrado e Cubo

---
+++ 4 - Desafios Iniciais/Desafio2/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.WriteLine("Digite o total de linhas:");
+var n = int.Parse(Console.ReadLine());
+
+int inicio = 1;
+
+// for (int i = 0; i < n; i++)
+// {
+//     var quadrado = inicio * inicio;
+//     var cubo = inicio * inicio * inicio;
+
+//     Console.WriteLine($"{inicio} {quadrado} {cubo}");
+
+//     inicio++;
+// }
+
+for (int i = 0; i < n; i++)
+{
+    var quadrado = Math.Pow(inicio, 2);
+    var cubo = Math.Pow(inicio, 3);
+
+    Console.WriteLine($"{inicio} {quadrado} {cubo}");
+
+    inicio++;
+}
