26f75c77f956568197046438ce461fd658d22d29
Lucas <lcseferreira@hotmail.com>
2022-03-05 11:10:04 -0300

    6 - Trabalhando com Arquivos e Streams em C# - class File

---
+++ 6 - Trabalhando com Arquivos e Streams em C#/FileAndFileInfo/Program.cs
@@ -0,0 +1,10 @@
+﻿// Caminho do arquivo
+var path = Path.Combine(Environment.CurrentDirectory, "teste.txt");
+// var path = Environment.CurrentDirectory + @"\teste.txt";
+
+// Criando um arquivo e usando o using por causa do IDisposable
+using var sw = File.CreateText(path);
+sw.WriteLine("Está é a linha 1 do arquivo");
+// sw.Flush();
+sw.WriteLine("Está é a linha 2 do arquivo");
+// sw.Flush();
