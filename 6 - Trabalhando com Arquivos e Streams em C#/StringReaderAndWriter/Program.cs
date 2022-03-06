da47b3e6b28adbfce79b4ec96e43f94795ebb179
Lucas <lcseferreira@hotmail.com>
2022-03-06 14:36:21 -0300

    6 - Trabalhando com Arquivos e Streams em C# - StringReader

---
+++ 6 - Trabalhando com Arquivos e Streams em C#/StringReaderAndWriter/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Text;
+
+// 
+var sb = new StringBuilder();
+sb.AppendLine("Primeira linha");
+sb.AppendLine("Segunda linha");
+sb.AppendLine("Terceira linha");
+
+using var sr = new StringReader(sb.ToString());
+var buffer = new char[10];
+var tamanho = 0;
+
+// do
+// {
+//     buffer = new char[10];
+//     tamanho = sr.Read(buffer);
+//     Console.Write(string.Join("", buffer));
+// } while (tamanho >= buffer.Length);
+
+do
+{
+    Console.WriteLine(sr.ReadLine());
+} while (sr.Peek() >= 0);
